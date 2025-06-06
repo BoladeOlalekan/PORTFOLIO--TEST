5965483c8932f3e12b8007a2e007fb8697bae007
Bolade Olalekan <lekanseyibolade@gmail.com>
2025-06-07 00:06:31 +0100

    Modified desktop view for a better and cleaner experience

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,13 @@
 const open = document.querySelector('.hamburger-open');
 const closeBtn = document.querySelector('.hamburger-close');
-const navList = document.getElementById('nav-list');
-const body = document.querySelector('body');
+const nav = document.querySelector('nav');
 
-open.addEventListener('click', () => {
+open.addEventListener('click', ()=> {
     alert('Menu opened');
-    navList.classList.add('show-nav');
+    nav.classList.add('show-nav');
 });
 
-closeBtn.addEventListener('click', () => {
+closeBtn.addEventListener('click', ()=> {
+    alert('Menu closed');
     nav.classList.remove('show-nav');
 });
