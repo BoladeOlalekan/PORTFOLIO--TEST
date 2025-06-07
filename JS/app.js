35d0760aac00569e73bddf676f3449d45902e713
Bolade Olalekan <lekanseyibolade@gmail.com>
2025-06-07 19:22:47 +0100

    Resolved hamburger menu button

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,41 @@
-const open = document.querySelector('.hamburger-open');
+const openBtn = document.querySelector('.hamburger-open');
 const closeBtn = document.querySelector('.hamburger-close');
-const nav = document.querySelector('nav');
+const navBar = document.querySelector('nav');
+const home = document.querySelector('.top-li');
+const skills = document.querySelector('.skills');
+const experience = document.querySelector('.experience');
+const education = document.querySelector('.education');
+const certification = document.querySelector('.certification');
+const connect = document.querySelector('.connect');
 
-open.addEventListener('click', ()=> {
-    alert('Menu opened');
-    nav.classList.add('show-nav');
-});
+openBtn.addEventListener('click' , () => {
+    navBar.style.transform = "translateX(0%)";
+})
 
-closeBtn.addEventListener('click', ()=> {
-    alert('Menu closed');
-    nav.classList.remove('show-nav');
-});
+closeBtn.addEventListener('click' , () => {
+    navBar.style.transform = 'translateX(100%)';
+})
+
+home.addEventListener('click' , () => {
+    navBar.style.transform = 'translateX(100%)';
+})
+
+skills.addEventListener('click' , () => {
+    navBar.style.transform = 'translateX(100%)';
+})
+
+experience.addEventListener('click' , () => {
+    navBar.style.transform = 'translateX(100%)';
+})
+
+education.addEventListener('click' , () => {
+    navBar.style.transform = 'translateX(100%)';
+})
+
+certification.addEventListener('click' , () => {
+    navBar.style.transform = 'translateX(100%)';
+})
+
+connect.addEventListener('click' , () => {
+    navBar.style.transform = 'translateX(100%)';
+})
